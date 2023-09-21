c4705d8a689948e8b65bb3c697fcc6d94769fa78
RayanGosling <mr.avdonin@inbox.ru>
2023-09-21 11:00:43 +0300

    Fixed variables

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        int Balance = 100;
-        int Deposit = 1100;
-        int Bonus;
-        boolean Operable = Deposit > 1000;
-        if (Operable) {
-            Bonus = Deposit / 100;
+        int balance = 100;
+        int deposit = 1100;
+        int bonus;
+        boolean operable = deposit > 1000;
+        if (operable) {
+            bonus = deposit / 100;
         } else {
-            Bonus = 0;
+            bonus = 0;
         }
-        int FinalBalance = Balance + Deposit + Bonus;
-        System.out.println("Баланс:" + Balance);
-        System.out.println("Сумма пополнения:" + Deposit);
-        System.out.println("Бонус:" + Bonus);
-        System.out.println("Итоговый баланс:" + FinalBalance);
+        int finalbalance = balance + deposit + bonus;
+        System.out.println("Баланс:" + balance);
+        System.out.println("Сумма пополнения:" + deposit);
+        System.out.println("Бонус:" + bonus);
+        System.out.println("Итоговый баланс:" + finalbalance);
 
     }
 }
